8061858ea9075a7fbd8a287172bdf26ae01e54f4
Genadi Kolev <genadimkolev@gmail.com>
2024-03-20 17:16:58 +0200

    add printFileContnent practice

---
+++ practice/files/printFileContent.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <iostream>
+
+class FileHandler {
+   private:
+    std::ifstream* file;
+    void checkFile() {
+        if (file == nullptr) throw std::runtime_error("File is not open");
+    }
+
+   public:
+    FileHandler(const char* path) {
+        file = new std::ifstream(path);
+
+        if (!file->is_open() || file->bad()) file = nullptr;
+    }
+    ~FileHandler() {
+        if (file == nullptr) return;
+        file->close();
+        delete file;
+    }
+
+    void writeTo(std::ostream& output) {
+        checkFile();
+
+        while (true) {
+            char ch = file->get();
+            if (file->eof() || file->fail()) break;
+            output << ch;
+        }
+        output << std::endl;
+
+        file->clear();
+        file->seekg(std::ios::beg);
+    }
+};
+
+int main() {
+    FileHandler handler("../practice/files/printFileContent.cpp");
+    handler.writeTo(std::cout);
+
+    std::ofstream copyFile("../practice/tmp/copy.cpp");
+    if (copyFile.is_open()) handler.writeTo(copyFile);
+
+    return 0;
+}
