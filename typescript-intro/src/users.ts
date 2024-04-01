1ab84988a990aab162df6e387f42670c24b290f6
Genadi Kolev <genadimkolev@gmail.com>
2024-04-01 18:50:03 +0300

    sync typescript-intro project

--- typescript-intro/src/users.ts
+++ typescript-intro/src/users.ts
@@ -1,39 +1,77 @@
-type IdType = number;
-type CoutnryType = "BG" | "US" | "GR" | "EN" | "DE";
+export type IdType = number;
+export type CountryType = "BG" | "DE" | "FR" | "GB" | "US"; // ...
 
 export interface Person {
-  id: IdType;
-  firstName: string;
-  lastName: string;
-  email: string;
-  contact?: Contact;
+    id: IdType;
+    firstName: string;
+    lastName: string;
+    email: string;
+    contact?: Contact;
+    readonly salutation: string;
 }
 
 export interface Contact {
-  country: CoutnryType;
-  city?: string;
-  address?: string;
-  phone?: string;
+    country: CountryType;
+    city?: string;
+    address?: string;
+    phone?: string;
 }
 
 export enum Role {
-  Author = 0,
-  Reader,
+    Author = 1, Reader, Admin
 }
 
 export interface User extends Person {
-  password: string;
-  roles: Role[]; // same as Array<Role>
+    password: string;
+    roles: Role[]; // Array<Role>;
 }
 
-export class UserBase implements User {
-  constructor(
-    public password: string,
-    public roles: Role[],
-    public id: number,
-    public firstName: string,
-    public lastName: string,
-    public email: string,
-    public contact?: Contact | undefined
-  ) {}
+export class PersonImpl implements Person {
+    constructor(
+        public id: IdType,
+        public firstName: string,
+        public lastName: string,
+        public email: string,
+        public contact?: Contact,
+    ) { }
+    get salutation() {
+        return `Hello Person: ${this.firstName} ${this.firstName} [${this.email}]`;
+    }
+}
+
+
+export type UserCreateDto = Omit<User, "id">
+
+export class UserDto implements UserCreateDto {
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public email: string,
+        public password: string,
+        public contact?: Contact,
+        public roles: Role[] = [Role.Reader]
+    ) { }
+    get salutation() {
+        return `Hi ${this.firstName} ${this.firstName} [${this.email}] in roles: ${this.roles.map(r => Role[r]).join(', ')}`;
+    }
+}
+
+export class UserImpl extends UserDto implements User {
+    constructor(
+        public id: IdType,
+        firstName: string,
+        lastName: string,
+        email: string,
+        password: string,
+        contact?: Contact,
+        roles: Role[] = [Role.Reader]
+    ) {
+        super(
+            firstName,
+            lastName,
+            email,
+            password,
+            contact,
+            roles);
+    }
 }
