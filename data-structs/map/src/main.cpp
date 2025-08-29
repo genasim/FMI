0e5e1377316a50a703d383cbf84d364fe012fec5
Genadi Kolev <genadimkolev@gmail.com>
2025-08-29 09:42:54 +0300

    add map data structure implementation

--- data-structs/map/src/main.cpp
+++ data-structs/map/src/main.cpp
@@ -9,25 +9,31 @@
 
 TEST_CASE("Map instantiation") {
     SUBCASE("Default constructor") {
-        Map<int, std::string> map;
+        map<int, std::string> map;
         CHECK(map.size() == 0);
         CHECK(map.empty());
+    }
+
+    SUBCASE("Initializer list") {
+        map<int, std::string> map({{1, "1"}, {2, "2"}, {3, "3"}, {4, "4"}, {5, "5"}});
+        CHECK(map.size() == 5);
+        CHECK_FALSE(map.empty());
+
+        CHECK(map.has_key(1));
+        CHECK(map.has_key(2));
+        CHECK(map.has_key(3));
+        CHECK(map.has_key(4));
+        CHECK(map.has_key(5));
     }
 }
 
 TEST_CASE("Map insertion") {
-    Map<int, std::string> map;
-    map.insert(1, "one");
-    map.insert(2, "two");
-    map.insert(3, "three");
-    map.insert(4, "four");
-    map.insert(5, "five");
-
+    map<int, std::string> map({{1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}});
     CHECK(map.size() == 5);
     CHECK_FALSE(map.empty());
 
     SUBCASE("Inserting duplicate key") {
-        CHECK_FALSE(map.insert(1, "uno"));
+        CHECK_FALSE(map.insert(1, "uno").second);
         CHECK(map.size() == 5);
 
         CHECK_FALSE(map.at(1).second == "one");
@@ -35,21 +41,42 @@ TEST_CASE("Map insertion") {
     }
 
     SUBCASE("Inserting pair") {
-        CHECK(map.insert({6, "six"}));
+        CHECK(map.insert({6, "six"}).second);
         CHECK(map.size() == 6);
 
-        CHECK_FALSE(map.insert({6, "seis"}));
+        CHECK_FALSE(map.insert({6, "seis"}).second);
         CHECK(map.size() == 6);
     }
 }
 
-TEST_CASE()
+TEST_CASE("Map removal") {
+    SUBCASE("Removing key with no children") {
+        map<int, std::string> map({{1, "1"}, {2, "2"}, {3, "3"}});
+        CHECK(map.remove(3));
+        CHECK(map.size() == 2);
+    }
+
+    SUBCASE("Removing key with right child") {
+        map<int, std::string> map({{1, "1"}, {2, "2"}, {3, "3"}});
+        CHECK(map.remove(2));
+        CHECK(map.size() == 2);
+    }
+
+    SUBCASE("Removing key with left child") {
+        map<int, std::string> map({{9, "9"}, {4, "4"}, {3, "3"}});
+        CHECK(map.remove(9));
+        CHECK(map.size() == 2);
+    }
+
+    SUBCASE("Removing key with both children") {
+        map<int, std::string> map({{10, "10"}, {9, "9"}, {15, "15"}, {12, "12"}, {14, "14"}});
+        CHECK(map.remove(10));
+        CHECK(map.size() == 4);
+    }
+}
 
 TEST_CASE("Map iteration") {
-    Map<int, std::string> map;
-    map.insert(1, "one");
-    map.insert(2, "two");
-    map.insert(3, "three");
+    const map<int, std::string> map({{2, "two"}, {1, "one"}, {3, "three"}});
 
     SUBCASE("Manual iteration") {
         auto it = map.begin();
@@ -66,15 +93,6 @@ TEST_CASE("Map iteration") {
 
         ++it;
         CHECK(it == map.end());
-    }
-
-    SUBCASE("Range-based for loop") {
-        int i = 1;
-        for (const auto& [key, value] : map) {
-            CHECK(key == i);
-            CHECK(value == std::to_string(i));
-            ++i;
-        }
     }
 }
 
--- data-structs/map/src/public/map.hpp
+++ data-structs/map/src/public/map.hpp
@@ -1,19 +1,25 @@
 #pragma once
 
 #include <functional>
+#include <initializer_list>
 #include <stack>
+#include <stdexcept>
+#include <utility>
 
 template <class Key, class Value, class Compare = std::less<Key>>
-class Map {
+class map {
    public:
-    Map();
-    explicit Map(const Compare& comparator);
-    Map(const Map& other);
-    Map& operator=(const Map& other);
-    ~Map();
+    struct const_iterator;
 
-    bool insert(const std::pair<Key, Value>& newPair);
-    bool insert(const Key& key, const Value& value);
+    map();
+    explicit map(const Compare& comparator);
+    map(const std::initializer_list<std::pair<Key, Value>>& init_list);
+    map(const map& other);
+    map& operator=(const map& other);
+    ~map();
+
+    std::pair<const_iterator, bool> insert(const std::pair<Key, Value>& newPair);
+    std::pair<const_iterator, bool> insert(const Key& key, const Value& value);
     bool has_key(const Key& key) const noexcept;
     bool remove(const Key& key);
 
@@ -56,33 +62,45 @@ class Map {
     size_t _size;
     Compare comparator;
 
+    Node** findMinNodeFor(Node** curr) const noexcept;
+
     void _free(Node* curr);
     Node* _copy(Node* curr);
 };
 
 template <class Key, class Value, class Compare>
-inline Map<Key, Value, Compare>::Map() : root(nullptr), _size(0), comparator(Compare()) {}
+inline map<Key, Value, Compare>::map() : root(nullptr), _size(0), comparator(Compare()) {}
 
 template <class Key, class Value, class Compare>
-inline Map<Key, Value, Compare>::Map(const Compare& comparator)
+inline map<Key, Value, Compare>::map(const Compare& comparator)
     : root(nullptr), _size(0), comparator(comparator) {}
 
 template <class Key, class Value, class Compare>
-inline Map<Key, Value, Compare>::Map(const Map& other)
+inline map<Key, Value, Compare>::map(const std::initializer_list<std::pair<Key, Value>>& init_list)
+    : map() {
+    for (const auto& pair : init_list) {
+        insert(pair);
+    }
+}
+
+template <class Key, class Value, class Compare>
+inline map<Key, Value, Compare>::map(const map& other)
     : root(_copy(other.root)), _size(other._size), comparator(other.comparator) {}
 
 template <class Key, class Value, class Compare>
-inline Map<Key, Value, Compare>::~Map() {
+inline map<Key, Value, Compare>::~map() {
     _free(root);
 }
 
 template <class Key, class Value, class Compare>
-inline bool Map<Key, Value, Compare>::insert(const std::pair<Key, Value>& newPair) {
+inline std::pair<typename map<Key, Value, Compare>::const_iterator, bool>
+map<Key, Value, Compare>::insert(const std::pair<Key, Value>& newPair) {
     return insert(newPair.first, newPair.second);
 }
 
 template <class Key, class Value, class Compare>
-inline bool Map<Key, Value, Compare>::insert(const Key& key, const Value& value) {
+inline std::pair<typename map<Key, Value, Compare>::const_iterator, bool>
+map<Key, Value, Compare>::insert(const Key& key, const Value& value) {
     Node** current = &root;
     while (*current) {
         if (comparator(key, (*current)->data.first)) {
@@ -91,18 +109,18 @@ inline bool Map<Key, Value, Compare>::insert(const Key& key, const Value& value)
             current = &(*current)->right;
         } else {
             (*current)->data.second = value;
-            return false;
+            return {const_iterator(*current), false};
         }
     }
 
     *current = new Node(std::make_pair(key, value));
     ++_size;
-    return true;
+    return {const_iterator(*current), true};
 }
 
 template <class Key, class Value, class Compare>
-inline bool Map<Key, Value, Compare>::has_key(const Key& key) const noexcept {
-    Node** current = &root;
+inline bool map<Key, Value, Compare>::has_key(const Key& key) const noexcept {
+    Node* const* current = &root;
     while (*current) {
         if (comparator(key, (*current)->data.first)) {
             current = &(*current)->left;
@@ -117,7 +135,41 @@ inline bool Map<Key, Value, Compare>::has_key(const Key& key) const noexcept {
 }
 
 template <class Key, class Value, class Compare>
-inline std::pair<Key, Value>& Map<Key, Value, Compare>::at(const Key& key) {
+inline bool map<Key, Value, Compare>::remove(const Key& key) {
+    Node** current = &root;
+    while (*current) {
+        if (comparator((*current)->data.first, key))
+            current = &(*current)->right;
+        else if (comparator(key, (*current)->data.first))
+            current = &(*current)->left;
+        else
+            break;
+    }
+
+    if (!(*current)) return false;
+
+    Node* toDelete = *current;
+    if (!toDelete->left && !toDelete->right)
+        *current = nullptr;
+    else if (!toDelete->left)
+        *current = toDelete->right;
+    else if (!toDelete->right)
+        *current = toDelete->left;
+    else {
+        Node** minRight = findMinNodeFor(&toDelete->right);
+        *current = *minRight;
+        *minRight = (*minRight)->right;
+        (*current)->left = toDelete->left;
+        (*current)->right = toDelete->right;
+    }
+
+    delete toDelete;
+    --_size;
+    return true;
+}
+
+template <class Key, class Value, class Compare>
+inline std::pair<Key, Value>& map<Key, Value, Compare>::at(const Key& key) {
     Node** current = &root;
     while (*current) {
         if (comparator(key, (*current)->data.first)) {
@@ -133,29 +185,38 @@ inline std::pair<Key, Value>& Map<Key, Value, Compare>::at(const Key& key) {
 }
 
 template <class Key, class Value, class Compare>
-inline size_t Map<Key, Value, Compare>::size() const noexcept {
+inline size_t map<Key, Value, Compare>::size() const noexcept {
     return _size;
 }
 
 template <class Key, class Value, class Compare>
-inline bool Map<Key, Value, Compare>::empty() const noexcept {
+inline bool map<Key, Value, Compare>::empty() const noexcept {
     return _size == 0;
 }
 
 template <class Key, class Value, class Compare>
-inline typename Map<Key, Value, Compare>::const_iterator Map<Key, Value, Compare>::begin()
+inline typename map<Key, Value, Compare>::const_iterator map<Key, Value, Compare>::begin()
     const noexcept {
     return const_iterator(root);
 }
 
 template <class Key, class Value, class Compare>
-inline typename Map<Key, Value, Compare>::const_iterator Map<Key, Value, Compare>::end()
+inline typename map<Key, Value, Compare>::const_iterator map<Key, Value, Compare>::end()
     const noexcept {
     return const_iterator(nullptr);
 }
 
 template <class Key, class Value, class Compare>
-inline void Map<Key, Value, Compare>::_free(Node* current) {
+inline typename map<Key, Value, Compare>::Node** map<Key, Value, Compare>::findMinNodeFor(
+    Node** curr) const noexcept {
+    while ((*curr)->left) {
+        curr = &(*curr)->left;
+    }
+    return curr;
+}
+
+template <class Key, class Value, class Compare>
+inline void map<Key, Value, Compare>::_free(Node* current) {
     if (!current) return;
     _free(current->left);
     _free(current->right);
@@ -163,7 +224,7 @@ inline void Map<Key, Value, Compare>::_free(Node* current) {
 }
 
 template <class Key, class Value, class Compare>
-inline typename Map<Key, Value, Compare>::Node* Map<Key, Value, Compare>::_copy(Node* current) {
+inline typename map<Key, Value, Compare>::Node* map<Key, Value, Compare>::_copy(Node* current) {
     if (!current) return nullptr;
 
     Node* newNode = new Node(current->data);
@@ -174,7 +235,7 @@ inline typename Map<Key, Value, Compare>::Node* Map<Key, Value, Compare>::_copy(
 }
 
 template <class Key, class Value, class Compare>
-void Map<Key, Value, Compare>::const_iterator::push_left(Node* node) {
+void map<Key, Value, Compare>::const_iterator::push_left(Node* node) {
     while (node) {
         stack.push(node);
         node = node->left;
@@ -182,19 +243,19 @@ void Map<Key, Value, Compare>::const_iterator::push_left(Node* node) {
 }
 
 template <class Key, class Value, class Compare>
-Map<Key, Value, Compare>::const_iterator::const_iterator(Node* root) {
+map<Key, Value, Compare>::const_iterator::const_iterator(Node* root) {
     push_left(root);
 }
 
 template <class Key, class Value, class Compare>
-inline const typename Map<Key, Value, Compare>::Data&
-Map<Key, Value, Compare>::const_iterator::operator*() const {
+inline const typename map<Key, Value, Compare>::Data&
+map<Key, Value, Compare>::const_iterator::operator*() const {
     return stack.top()->data;
 }
 
 template <class Key, class Value, class Compare>
-inline typename Map<Key, Value, Compare>::const_iterator&
-Map<Key, Value, Compare>::const_iterator::operator++() {
+inline typename map<Key, Value, Compare>::const_iterator&
+map<Key, Value, Compare>::const_iterator::operator++() {
     Node* current = stack.top();
     stack.pop();
     push_left(current->right);
@@ -202,21 +263,21 @@ Map<Key, Value, Compare>::const_iterator::operator++() {
 }
 
 template <class Key, class Value, class Compare>
-inline typename Map<Key, Value, Compare>::const_iterator
-Map<Key, Value, Compare>::const_iterator::operator++(int) {
+inline typename map<Key, Value, Compare>::const_iterator
+map<Key, Value, Compare>::const_iterator::operator++(int) {
     auto temp = *this;
     ++(*this);
     return temp;
 }
 
 template <class Key, class Value, class Compare>
-inline bool Map<Key, Value, Compare>::const_iterator::operator!=(
+inline bool map<Key, Value, Compare>::const_iterator::operator!=(
     const const_iterator& other) const noexcept {
     return !(*this == other);
 }
 
 template <class Key, class Value, class Compare>
-inline bool Map<Key, Value, Compare>::const_iterator::operator==(
+inline bool map<Key, Value, Compare>::const_iterator::operator==(
     const const_iterator& other) const noexcept {
     return stack == other.stack;
 }
